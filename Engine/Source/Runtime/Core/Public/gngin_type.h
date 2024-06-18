2d8b4d39b96e6834520483d8267d6a0cab400c46
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2024-06-18 23:57:22 +0900

    [add] 型定義の共通ヘッダーファイルを追加

---
+++ Engine/Source/Runtime/Core/Public/gngin_type.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#if __cplusplus < 201103L
+    #error This header requires C++11 or higher
+#endif
+
+#include <cstdint>
+#include <cstdbool>
+
+#ifndef GNGIN_BOOL
+    #define GNGIN_BOOL bool
+#endif
+
+#ifndef GNGIN_TRUE
+    #define GNGIN_TRUE true
+#endif
+
+#ifndef GNGIN_FALSE
+    #define GNGIN_FALSE false
+#endif
+
+typedef char GnginChar8;
+typedef char16_t GnginChar16;
+typedef char32_t GnginChar32;
+
+typedef std::int8_t GnginSint8;
+typedef std::int16_t GnginSint16;
+typedef std::int32_t GnginSint32;
+typedef std::int64_t GnginSint64;
+
+typedef std::uint8_t GnginUint8;
+typedef std::uint16_t GnginUint16;
+typedef std::uint32_t GnginUint32;
+typedef std::uint64_t GnginUint64;
+
+typedef float GnginFloat32;
+typedef double GnginFloat64;
