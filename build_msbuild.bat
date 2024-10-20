ea7aceea001d02132b8d02f3eb77053948f8de64
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2024-10-20 22:28:53 +0900

    [fix] 自動ビルドテストの修正

---
+++ build_msbuild.bat
@@ -0,0 +1,11 @@
+setlocal
+
+mkdir Build
+mkdir Binaries
+
+cmake -B Build -S . -G "Visual Studio 17 2022"
+cd Build
+
+msbuild /p:Configuration=Release /p:Platform=x64 /t:Build ALL_BUILD.vcxproj
+
+D:\Qt\5.15.2\msvc2019_64\bin\windeployqt.exe ..\Binaries\Release\gEngineEditor.exe
