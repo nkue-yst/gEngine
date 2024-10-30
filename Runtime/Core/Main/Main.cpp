b4b7abec88588dd94512703bd51e00e865317ce7
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2024-10-30 23:54:21 +0900

    [add] Mainクラス、アプリ用エントリーポイントを追加

--- Runtime/Core/Main/Main.cpp
+++ Runtime/Core/Main/Main.cpp
@@ -3,6 +3,17 @@
 namespace gngin
 {
 
+Main::Main()
+{
+}
 
+Main::~Main()
+{
+}
+
+int32_t Main::Exec(int argc, char** argv)
+{
+    return 0;
+}
 
 }
--- Runtime/Core/Main/Main.h
+++ Runtime/Core/Main/Main.h
@@ -1,11 +1,25 @@
 #pragma once
 
+#include <cstdint>
+
 namespace gngin
 {
 
 class GENGINE_RUNTIME_API Main
 {
+public:
+    Main();
+    ~Main();
 
+    int32_t Exec(int argc, char** argv);
 };
 
 }
+
+#ifdef GN_BUILD_WITH_MAIN
+int main(int argc, char** argv)
+{
+    Main gn_main;
+    return gn_main.Exec();
+}
+#endif
